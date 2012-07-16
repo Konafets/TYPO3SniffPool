b1c91e85a13d3344864f0f05d1b609b54b4fb6a4
Stefano Kowalke <blueduck@gmx.net>
2012-07-16 15:58:10 +0200

    [Feature] New sniff to check spaces around concat op
    The new sniff checks if the concat operator is surrounded by spaces.
    
    The sniff is taken from CodeSniffer project and modified a little bit to fit our needs.
    
    Change-Id: I50a36e980b58b8fa0393fd7cfab122428ba89440
    Fixes: 37940
    Release: 0.0.4

---
+++ Sniffs/Strings/ConcatenationSpacingSniff.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * TYPO3_Sniffs_Strings_ConcatenationSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      http://pear.typo3.org
+ */
+
+/**
+ * TYPO3_Sniffs_Strings_ConcatenationSpacingSniff.
+ *
+ * Makes sure there are no spaces between the concatenation operator (.) and
+ * the strings being concatenated.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @version   Release: @package_version@
+ * @link      http://pear.typo3.org
+ */
+class TYPO3_Sniffs_Strings_ConcatenationSpacingSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_STRING_CONCAT);
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE
+            || $tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE
+        ) {
+            $message = 'Concat operator must be surrounded by spaces';
+            $phpcsFile->addError($message, $stackPtr, 'NoSpaceAroundConcat');
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
