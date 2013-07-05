f535b1315673e8e85f60a8cacbadd62e30297f08
Stefano Kowalke <blueduck@gmx.net>
2013-07-05 23:08:22 +0200

    [Feature] Detect usage of $_ in foreach loops
    Fixes: #12058
    Release: 0.0.7

---
+++ Sniffs/ControlStructures/UnusedVariableInForEachLoopSniff.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * TYPO3SniffPool_Sniffs_ControlStructures_UnusedVariableInForEachLoopSniff.
+ *
+ * PHP version 5
+ * TYPO3CMS
+ *
+ * @category  ControlStructures
+ * @package   TYPO3SniffPool
+ * @author    Andy Grunwald <andygrunwald@gmail.com>
+ * @copyright 2012 Andy Grunwald
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      http://pear.typo3.org
+ */
+/**
+ * Checks if a unused variable in a foreach loop is named $_.
+ *
+ * @category  ControlStructures
+ * @package   TYPO3SniffPool
+ * @author    Andy Grunwald <andygrunwald@gmail.com>
+ * @copyright 2012 Andy Grunwald
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @version   Release: @package_version@
+ * @link      http://pear.typo3.org
+ */
+class TYPO3SniffPool_Sniffs_ControlStructures_UnusedVariableInForEachLoopSniff implements PHP_CodeSniffer_Sniff
+{
+
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('PHP');
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_FOREACH);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $startToken = $tokens[$stackPtr]['parenthesis_opener'];
+        $endToken = $tokens[$stackPtr]['parenthesis_closer'];
+        $startToken = $phpcsFile->findNext(T_AS, $startToken, $endToken);
+
+        $valueToken = $phpcsFile->findNext(T_VARIABLE, $startToken, $endToken);
+        $tmpToken = $phpcsFile->findNext(T_VARIABLE, $valueToken + 1, $endToken);
+
+        // If $tmpToken is not false, the foreach loop uses $key => $value
+        $keyToken = false;
+        if ($tmpToken !== false) {
+            $keyToken = $valueToken;
+            $valueToken = $tmpToken;
+            unset($tmpToken);
+        }
+
+        $scopeOpener = $tokens[$stackPtr]['scope_opener'];
+        $scopeCloser = $tokens[$stackPtr]['scope_closer'];
+
+        // If a $key is used in foreach loop but not used in the foreach body
+        if ($keyToken !== false && $phpcsFile->findNext(T_VARIABLE, $scopeOpener, $scopeCloser, false, $tokens[$keyToken]['content']) === false) {
+            $message = 'The usage of the key variable %s is not necessary. Please remove this.';
+            $phpcsFile->addError($message, $stackPtr, 'KeyVariableNotNecessary', array($tokens[$keyToken]['content']));
+        }
+
+        // If the $value is named $_ AND used in the foreach body, this variable has to be renamed
+        if ($tokens[$valueToken]['content'] === '$_' && $phpcsFile->findNext(T_VARIABLE, $scopeOpener, $scopeCloser, false, $tokens[$valueToken]['content']) !== false) {
+            $message = 'The variable $_ is used in the foreach body. Please rename this variable to a more useful name.';
+            $phpcsFile->addError($message, $stackPtr, 'ValueVariableWrongName');
+
+        // If the $value is NOT named $_, but no one will use this in the foreach body, this variable has to be renamed
+        } elseif ($tokens[$valueToken]['content'] !== '$_' && $phpcsFile->findNext(T_VARIABLE, $scopeOpener, $scopeCloser, false, $tokens[$valueToken]['content']) === false) {
+            $message = 'The variable %s is NOT used in the foreach body. Please rename this variable to $_.';
+            $phpcsFile->addError($message, $stackPtr, 'ValueVariableNotUsed', array($tokens[$valueToken]['content']));
+        }
+    }
+}
+?>
---
+++ Tests/ControlStructures/UnusedVariableInForEachLoopUnitTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Unit test class for the UnusedVariableInForEachLoop sniff.
+ *
+ * PHP version 5
+ * TYPO3CMS
+ *
+ * @category  ControlStructures
+ * @package   TYPO3SniffPool
+ * @author    Andy Grunwald <andygrunwald@gmail.com>
+ * @copyright 2012 Andy Grunwald
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      http://pear.typo3.org
+ */
+/**
+ * Unit test class for the UnusedVariableInForEachLoop sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  ControlStructures
+ * @package   TYPO3SniffPool
+ * @author    Andy Grunwald <andygrunwald@gmail.com>
+ * @copyright 2012 Andy Grunwald
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @version   Release: @package_version@
+ * @link      http://pear.typo3.org
+ */
+class TYPO3SniffPool_Tests_ControlStructures_UnusedVariableInForEachLoopUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array(
+            6 => 1,
+            10 => 1,
+            18 => 1,
+            22 => 2,
+            30 => 1,
+        );
+    } //end getErrorList()
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+    } //end getWarningList()
+
+} //end class
+
+?>
