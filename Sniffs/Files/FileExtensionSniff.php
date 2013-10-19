16c654451eb30714c70f758508233cb5f1b39d33
Stefano Kowalke <blueduck@gmx.net>
2013-10-19 21:40:26 +0200

    Add a sniff to check for the correct file extension
    Fixes #7

---
+++ Sniffs/Files/FileExtensionSniff.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * TYPO3SniffPool_Sniffs_Files_FileExtensionSniff.
+ *
+ * PHP version 5
+ * TYPO3 version 4
+ *
+ * @category  Files
+ * @package   TYPO3SniffPool
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @copyright 2013 Stefano Kowalke
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      http://pear.typo3.org
+ */
+/**
+ * Checks if a PHP files has the extension .inc instead of .php
+ *
+ * @category  Files
+ * @package   TYPO3SniffPool
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @copyright 2013 Stefano Kowalke
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @version   Release: @package_version@
+ * @link      http://pear.typo3.org
+ */
+class TYPO3SniffPool_Sniffs_Files_FileExtensionSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('PHP');
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_OPEN_TAG);
+
+    }//end register()
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        // Make sure this is the first PHP open tag so we don't process
+        // the same file twice.
+        $prevOpenTag = $phpcsFile->findPrevious(T_OPEN_TAG, ($stackPtr - 1));
+        if ($prevOpenTag !== false) {
+            return;
+        }
+
+        $fileName  = $phpcsFile->getFileName();
+        $extension = substr($fileName, strrpos($fileName, '.'));
+
+        if ($extension !== '.php') {
+            $error = 'Extension for PHP files is always ".php". Found "' . $extension . '" file; use ".php" extension instead';
+            $phpcsFile->addError($error, $stackPtr, 'WrongFileExtension');
+        }
+    } //end process()
+
+} //end class
+
+?>
---
+++ Tests/Files/FileExtensionUnitTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Unit test class for the FileExtension sniff.
+ *
+ * PHP version 5
+ *
+ * @category  Files
+ * @package   TYPO3SniffPool
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @copyright 2013 Stefano Kowalke
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      http://pear.typo3.org
+ */
+
+/**
+ * Unit test class for the FileExtension sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  Files
+ * @package   TYPO3SniffPool
+ * @copyright 2013 Stefano Kowalke
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class TYPO3SniffPool_Tests_Files_FileExtensionUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array(
+                1 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
+
+?>
