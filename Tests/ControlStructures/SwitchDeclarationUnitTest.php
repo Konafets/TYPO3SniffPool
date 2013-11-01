cd4ce69df792bb42ef43e644cead5d48dd45e48f
Stefano Kowalke <blueduck@gmx.net>
2013-11-01 23:51:11 +0100

    Working draft. Need some cleanup.

---
+++ Tests/ControlStructures/SwitchDeclarationUnitTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Unit test class for the SwitchDeclaration sniff.
+ *
+ * PHP version 5
+ *
+ * @category  ControlStructures
+ * @package   TYPO3SniffPool
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2013 Stefano Kowalke
+ * @copyright 2006-2012 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      https://github.com/typo3-ci/TYPO3SniffPool
+ */
+
+/**
+ * Unit test class for the SwitchDeclaration sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  ControlStructures
+ * @package   TYPO3SniffPool
+ * @author    Stefano Kowalke <blueduck@gmx.net>
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2013 Stefano Kowalke
+ * @copyright 2006-2012 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @version   Release: @package_version@
+ * @link      https://github.com/typo3-ci/TYPO3SniffPool
+ */
+class TYPO3SniffPool_Tests_ControlStructures_SwitchDeclarationUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array(
+                27 => 1,
+                32 => 1,
+                33 => 1,
+                35 => 1,
+                47 => 1,
+                50 => 1,
+                61 => 1,
+                73 => 1,
+                78 => 1,
+                98 => 1,
+               );
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+
+    }//end getWarningList()
+
+
+}//end class
+
+?>
