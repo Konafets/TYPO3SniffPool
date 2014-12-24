3fe744e31ce2ee856f0d70bce728ca199948cb88
Stefano Kowalke <blueduck@gmx.net>
2014-12-24 01:31:03 +0100

    [TASK] Adjust AlwaysReturnSniff to the new comment tokenizer
    Resolves: #40

--- Sniffs/Scope/AlwaysReturnSniff.php
+++ Sniffs/Scope/AlwaysReturnSniff.php
@@ -3,19 +3,17 @@
  * TYPO3_Sniffs_Scope_AlwaysReturnSniff.
  *
  * PHP version 5
- * TYPO3 version 4
+ * TYPO3 CMS
  *
  * @category  Scope
  * @package   TYPO3_PHPCS_Pool
  * @author    Andy Grunwald <andygrunwald@gmail.de>
+ * @author    Stefano Kowalke <blueduck@gmx.net>
  * @copyright 2010 Andy Grunwald
+ * @copyright 2014 Stefano Kowalke
  * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
  * @link      https://github.com/typo3-ci/TYPO3SniffPool
  */
-
-if (class_exists('PHP_CodeSniffer_CommentParser_FunctionCommentParser', true) === false) {
-    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_CommentParser_FunctionCommentParser not found');
-}
 
 /**
  * Checks that a function / method always have a return value if it return something.
@@ -23,7 +21,9 @@ if (class_exists('PHP_CodeSniffer_CommentParser_FunctionCommentParser', true) ==
  * @category  Scope
  * @package   TYPO3_PHPCS_Pool
  * @author    Andy Grunwald <andygrunwald@gmail.de>
+ * @author    Stefano Kowalke <blueduck@gmx.net>
  * @copyright 2010 Andy Grunwald
+ * @copyright 2014 Stefano Kowalke
  * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
  * @version   Release: @package_version@
  * @link      https://github.com/typo3-ci/TYPO3SniffPool
@@ -36,14 +36,7 @@ class TYPO3SniffPool_Sniffs_Scope_AlwaysReturnSniff implements PHP_CodeSniffer_S
      *
      * @var array
      */
-    public $supportedTokenizes = array('PHP');
-
-    /**
-     * The function comment parser for the current method.
-     *
-     * @var PHP_CodeSniffer_Comment_Parser_FunctionCommentParser
-     */
-    protected $commentParser = null;
+    public $supportedTokenizers = array('PHP');
 
     /**
      * The current PHP_CodeSniffer_File object we are processing.
@@ -51,13 +44,6 @@ class TYPO3SniffPool_Sniffs_Scope_AlwaysReturnSniff implements PHP_CodeSniffer_S
      * @var PHP_CodeSniffer_File
      */
     protected $currentFile = null;
-
-    /**
-     * The position in the stack where the class token was found.
-     *
-     * @var int
-     */
-    protected $classToken = null;
 
     /**
      * The name of the method that we are currently processing.
@@ -89,53 +75,90 @@ class TYPO3SniffPool_Sniffs_Scope_AlwaysReturnSniff implements PHP_CodeSniffer_S
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
-        $this->currentFile = $phpcsFile;
         $tokens = $phpcsFile->getTokens();
+        $this->currentFile = $phpcsFile;
 
-        // Lets have a look if there is a doc comment. The doc comment could have a "@return void"
-        // If there is a "@return void" there must not be a "return".
-        $docComment = $this->getDocCommentOfFunction($phpcsFile, $stackPtr);
-        $className = '';
-
-        // Skip interfaces because the may have doc comments with @return annotations but no
-        // function body with a real return statement.
-        if ($this->classToken !== null) {
-            if ($tokens[$this->classToken]['code'] == T_INTERFACE) {
+        foreach ($tokens[$stackPtr]['conditions'] as $condPtr => $condition) {
+            // Skip interfaces because the may have doc comments with @return
+            // annotations but no function body with a real return statement.
+            if ($condition === T_INTERFACE) {
                 return;
             }
-            $className = $this->currentFile->getDeclarationName($this->classToken);
-            $className = strtolower(ltrim($className, '_'));
+        }
+
+        // Skip constructor and destructor.
+        $methodName      = $phpcsFile->getDeclarationName($stackPtr);
+        $isSpecialMethod = ($methodName === '__construct' || $methodName === '__destruct');
+
+        if ($isSpecialMethod === true) {
+            return;
         }
 
         // Skip files without scope openers
+        // This will skip also abstract functions, which is intended.
         if (!isset($tokens[$stackPtr]['scope_opener'])) {
             return;
+        }
+
+        $find   = PHP_CodeSniffer_Tokens::$methodPrefixes;
+        $find[] = T_WHITESPACE;
+
+        // Lets have a look if there is a doc comment.
+        // The doc comment could have a "@return void"
+        // If there is a "@return void" there must not be a "return".
+        $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1), null, true);
+        if ($tokens[$commentEnd]['code'] === T_COMMENT) {
+            // Inline comments might just be closing comments for
+            // control structures or functions instead of function comments
+            // using the wrong comment type. If there is other code on the line,
+            // assume they relate to that code.
+            $prev = $phpcsFile->findPrevious($find, ($commentEnd - 1), null, true);
+            if ($prev !== false && $tokens[$prev]['line'] === $tokens[$commentEnd]['line']) {
+                $commentEnd = $prev;
+            }
+        }
+
+        $return = null;
+        if ($tokens[$commentEnd]['code'] !== T_DOC_COMMENT_CLOSE_TAG
+            && $tokens[$commentEnd]['code'] !== T_COMMENT
+        ) {
+            $return = null;
+        } else {
+            $commentStart = $tokens[$commentEnd]['comment_opener'];
+
+            foreach ($tokens[$commentStart]['comment_tags'] as $tag) {
+                if ($tokens[$tag]['content'] === '@return') {
+                    $return = $tag;
+                }
+            }
         }
 
         $start = $tokens[$stackPtr]['scope_opener'];
         $end   = $tokens[$stackPtr]['scope_closer'];
 
-        // Skip constructor and destructor.
-        $methodName      = strtolower(ltrim($this->methodName, '_'));
-        $isSpecialMethod = ($this->methodName === '__construct' || $this->methodName === '__destruct');
-        if ($isSpecialMethod === true || ($className !== '' && $methodName === $className)) {
-            return;
-        }
-
-        if ($docComment !== null) {
-            $returnContent = $this->getValueOfReturnTag();
+        if ($return !== null) {
+            $tagValue = $phpcsFile->findNext(T_DOC_COMMENT_STRING, $return);
+            $returnContent = $tokens[$tagValue]['content'];
 
             // If there is "@return void" defined in doc block comment
             // and there is a non empty return statement (e.g. return 5;)
             if (strtolower($returnContent) === 'void' && $this->checkAvailableReturnStatement($tokens, $start, $end) === true) {
-                $error = 'This function must not have a return value because "@return void" is defined in doc comment.';
-                $phpcsFile->addError($error, $stackPtr, 'ReturnStatementInVoidFunction');
+                $error = 'The function "%s" must not have a return value because "@return void" is defined in doc comment.';
+                $phpcsFile->addError(
+                    $error,
+                    $stackPtr,
+                    'ReturnStatementInVoidFunction',
+                    array($methodName)
+                );
 
                 // If there is "@return int" or something like this defined in doc block comment
                 // and there is a empty return statement
-            } elseif ($returnContent !== null && strtolower($returnContent) !== 'void' && $this->checkAvailableReturnStatement($tokens, $start, $end, false) === true) {
-                $error = 'This function must not have a empty return value because "@return %s" is defined in doc comment.';
-                $errorData = array($returnContent);
+            } elseif ($returnContent !== null
+                && strtolower($returnContent) !== 'void'
+                && $this->checkAvailableReturnStatement($tokens, $start, $end, false) === true
+            ) {
+                $error = 'Found an empty return statement "return;" inside the function "%s", but also a "@return %s" in doc comment. Change the doc declaration to "@return void" or adjust the return statement. ';
+                $errorData = array($methodName, $returnContent);
                 $phpcsFile->addError($error, $stackPtr, 'EmptyReturnStatementInNonVoidFunction', $errorData);
             }
 
@@ -440,114 +463,4 @@ class TYPO3SniffPool_Sniffs_Scope_AlwaysReturnSniff implements PHP_CodeSniffer_S
 
         return $result;
     }
-
-    /**
-     * Ths function is mainly copied from PEAR_Sniffs_Commenting_FunctionCommentSniff.
-     * THX for this!
-     *
-     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                  $stackPtr  The position of the current token in the stack passed in $tokens.
-     *
-     * @return mixed
-     */
-    protected function getDocCommentOfFunction(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        $find = array(
-            T_COMMENT,
-            T_DOC_COMMENT,
-            T_CLASS,
-            T_FUNCTION,
-            T_OPEN_TAG,
-        );
-
-        $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1));
-
-        if ($commentEnd === false) {
-            return null;
-        }
-
-        $tokens = $phpcsFile->getTokens();
-
-            // If the token that we found was a class or a function, then this
-            // function has no doc comment.
-        $code = $tokens[$commentEnd]['code'];
-
-            // If the comment a "//" comment, get out of here
-        if ($code === T_COMMENT) {
-            return null;
-
-            // If there is no doc comment block, get out of here, too
-        } else if ($code !== T_DOC_COMMENT) {
-            return null;
-        }
-
-            // If there is any code between the function keyword and the doc block
-            // then the doc block is not for us.
-        $ignore    = PHP_CodeSniffer_Tokens::$scopeModifiers;
-        $ignore[]  = T_STATIC;
-        $ignore[]  = T_WHITESPACE;
-        $ignore[]  = T_ABSTRACT;
-        $ignore[]  = T_FINAL;
-        $prevToken = $phpcsFile->findPrevious($ignore, ($stackPtr - 1), null, true);
-        if ($prevToken !== $commentEnd) {
-            return null;
-        }
-
-        $this->classToken = null;
-        foreach ($tokens[$stackPtr]['conditions'] as $condPtr => $condition) {
-            if ($condition === T_CLASS || $condition === T_INTERFACE) {
-                $this->classToken = $condPtr;
-                break;
-            }
-        }
-
-            // If the first T_OPEN_TAG is right before the comment, it is probably
-            // a file comment.
-        $commentStart = ($phpcsFile->findPrevious(T_DOC_COMMENT, ($commentEnd - 1), null, true) + 1);
-        $prevToken    = $phpcsFile->findPrevious(T_WHITESPACE, ($commentStart - 1), null, true);
-        if ($tokens[$prevToken]['code'] === T_OPEN_TAG) {
-            // Is this the first open tag?
-            if ($stackPtr === 0 || $phpcsFile->findPrevious(T_OPEN_TAG, ($prevToken - 1)) === false) {
-                return null;
-            }
-        }
-
-        $comment           = $phpcsFile->getTokensAsString($commentStart, ($commentEnd - $commentStart + 1));
-        $this->methodName  = $phpcsFile->getDeclarationName($stackPtr);
-
-        try {
-            $this->commentParser = new PHP_CodeSniffer_CommentParser_FunctionCommentParser($comment, $phpcsFile);
-            $this->commentParser->parse();
-        } catch (PHP_CodeSniffer_CommentParser_ParserException $e) {
-            return null;
-        }
-
-        $comment = $this->commentParser->getComment();
-        if (is_null($comment) === true) {
-            return null;
-        }
-
-        return $this->commentParser;
-    }
-
-    /**
-     * Process the return comment of this function comment.
-     *
-     * @return void
-     */
-    protected function getValueOfReturnTag()
-    {
-        $returnContent = $tmpContent = null;
-        $pairElement = $this->commentParser->getReturn();
-        if ($pairElement instanceof PHP_CodeSniffer_CommentParser_AbstractDocElement) {
-            $tmpContent = trim($this->commentParser->getReturn()->getRawContent());
-        }
-
-        if ($tmpContent !== null) {
-            $returnContent = $tmpContent;
-        }
-
-        return $returnContent;
-    }
 }
-?>
