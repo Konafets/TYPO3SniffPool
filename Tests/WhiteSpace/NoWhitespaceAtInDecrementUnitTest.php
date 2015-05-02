a803331b3786b3bba5f57a513cf8f7e35b346d61
Stefano Kowalke <blueduck@gmx.net>
2015-05-02 16:35:16 +0200

    [TASK] Add an Whitespace.IncDecrementSniff
    We reimplement the same functionality again. It was removed with
    the former AssignmentAndComparison sniff.
    
    Closes: #61

---
+++ Tests/WhiteSpace/NoWhitespaceAtInDecrementUnitTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Unit test class for the NoWhitespaceAtInDecrementTest sniff.
+ *
+ * PHP version 5
+ *
+ * @category  Whitespace
+ * @package   TYPO3SniffPool
+ * @author    Andy Grunwald <andygrunwald@gmail.com>
+ * @author    Stefano Kowalke <blueduck@mailbox.org>
+ * @copyright 2010 Andy Grunwald
+ * @copyright 2015 Stefano Kowalke
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      https://github.com/typo3-ci/TYPO3SniffPool
+ */
+/**
+ * Unit test class for the NoWhitespaceAtInDecrementTest sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  Whitespace
+ * @package   TYPO3SniffPool
+ * @author    Andy Grunwald <andygrunwald@gmail.com>
+ * @author    Stefano Kowalke <blueduck@mailbox.org>
+ * @copyright 2010 Andy Grunwald
+ * @copyright 2015 Stefano Kowalke
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @link      https://github.com/typo3-ci/TYPO3SniffPool
+ */
+class TYPO3SniffPool_Tests_WhiteSpace_NoWhitespaceAtInDecrementUnitTest extends AbstractSniffUnitTest
+{
+
+
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList()
+    {
+        return array();
+
+    }//end getErrorList()
+
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array(
+                4  => 1,
+                8  => 1,
+                12 => 1,
+                16 => 1,
+                22 => 1,
+                30 => 1,
+                38 => 1,
+                46 => 1,
+                51 => 1,
+                53 => 1,
+                55 => 1,
+                57 => 1,
+               );
+
+    }//end getWarningList()
+
+
+}//end class
